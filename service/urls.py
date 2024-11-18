15a6f15f15cecca8531e3125a5a515807a574e0c
vissutagunawan <vglim3653@gmail.com>
2024-11-18 21:18:44 +0700

    debug

--- service/urls.py
+++ service/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'service'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('home/', views.homepage, name='homepage'),
     path('subcategory/pengguna/', views.subcategory_detail_pengguna, name='subcategory_detail_pengguna'),
     path('subcategory/pekerja/', views.subcategory_detail_pekerja, name='subcategory_detail_pekerja'),
     path('worker/worker_id/', views.worker_profile, name='worker_profile'),
--- sijarta/settings.py
+++ sijarta/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'discount',
-    'service.apps.ServiceConfig',
+    'service',
     'authentication',
     'user_profile'
 ]
